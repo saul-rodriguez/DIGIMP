422d3f5d4e60c99b3d4192855562bdb14db237cb
saul <saul@s2720.ict.kth.se>
2024-06-04 11:55:53 +0200

    ASKADIG ready for fpga tests

--- ASKA_DIG/FPGA_TESTS/top.v
+++ ASKA_DIG/FPGA_TESTS/top.v
@@ -1,6 +1,6 @@
 
 /*********************************************/
-/* ASKS NPG TEST IN FPGA ULX3                */
+/* ASKA DIG TEST IN FPGA ULX3                */
 /*                                           */
 /*  Mapping:                                 */
 /* 											 */
@@ -14,21 +14,9 @@
 /*                                           */
 /*********************************************/
 
-`include "aska_npg.v"
+`include "aska_dig.v"
 
 `define COUNT20K 625
-
-`define AMPLITUDE 50
-`define FREQ 2000
-`define PHASE 4
-`define RAMP  50
-`define RAMP_FACTOR (`AMPLITUDE*16)/`RAMP
-//`define RAMP_FACTOR 0
-`define ON_TIME  50
-`define OFF_TIME  50
-`define ELE1  32'b0000_0000_0000_0000_1000_0000_0000_0000
-`define ELE2  32'b0000_0000_0000_0000_0100_0000_0000_0000
-
 
 module top (
 	//input [3:0] sw, 	
@@ -37,10 +25,11 @@ module top (
 	output [7:0] led,	
 	input [6:0] btn, // [3:1] are active high, [0] is active low	
 	input clk_25mhz,
-	output wifi_gpio0    
-);
+	output wifi_gpio0);
     
-	/*ULX3 related wiring*/
+	/***********************/
+	/* ULX3 related wiring */
+	/***********************/
 
 	assign wifi_gpio0 = 1'b1;
 	
@@ -70,8 +59,19 @@ module top (
 	
 	reg clk_20khz;
 
-	always @(posedge clk_25mhz) begin
-		if (clk_counter < `COUNT20K) begin
+	//inputs
+	wire resetn;
+	wire enable;
+    
+	wire SPI_Clk;
+	wire SPI_MOSI;
+	wire SPI_CS;
+	wire porborn;
+
+	always @(posedge clk_25mhz or negedge resetn) begin
+		if (resetn == 1'b0) begin
+			clk_20khz <= 1'b0;
+		end else if (clk_counter < `COUNT20K) begin
 			clk_counter <= clk_counter + 1;		
 		end else begin
 			clk_counter <= 0;
@@ -85,42 +85,23 @@ module top (
 	
 	assign gn[0] = clk_20khz;	
 
-/* Instantiation of NPG*/
-	
-	//inputs
-	wire resetn;
-	wire enable;
-    wire [5:0] amplitude; //0 - 50 mA
-    wire [11:0] freq; // 4.88 Hz (4095) - 50 Hz (400)
-    wire [2:0] phaseDuration;
-    wire [5:0] ramp;
-    wire [9:0] ramp_factor;
-    wire [7:0] ON_time; // up to 4s (200 for 50 Hz)ramp = `RAMP;    
-    wire [9:0] OFF_time; // up to 12s (600 for 50 Hz)
-    wire [`ELEC_NUM:0] electrode1;
-    wire [`ELEC_NUM:0] electrode2;
-	    
+/******************************/
+/* Instantiation of aska_dig  */
+/******************************/
+		    
 	//Outputs
 	wire [`ELEC_NUM:0] up_switches;
     wire [`ELEC_NUM:0] down_switches;
     wire [5:0] DAC;
     wire pulse_active;
 
-
 	assign resetn = gp[0];
-	assign enable = gp[1];
+	assign porborn = gp[1];
+	assign SPI_Clk = gp[2];
+	assign SPI_MOSI = gp[3];
+	assign SPI_CS = gp[4];
 
-	assign amplitude = `AMPLITUDE;
-	assign freq = `FREQ;
-	assign phaseDuration = `PHASE;
-	assign ramp = `RAMP;
-    assign ramp_factor = `RAMP_FACTOR;
-    assign ON_time = `ON_TIME;
-    assign OFF_time = `OFF_TIME;
-    assign electrode1 = `ELE1;
-    assign electrode2 = `ELE2;
-
-
+//Change outputs to test different electrodes
 /*
 	assign gn[3:1] = up_switches[2:0];
 	assign gn[6:4] = down_switches[2:0];
@@ -138,24 +119,16 @@ module top (
 	assign gn[11] = DAC[4];
 	assign gn[12] = DAC[5];
 	assign gn[13] = pulse_active;
-		
-aska_npg UUT (
-    .clk(clk_20khz),
-    .resetn(resetn),
-    .amplitude(amplitude),
-    .freq(freq),
-    .phaseDuration(phaseDuration),
-    .ramp(ramp),
-    .ramp_factor(ramp_factor),
-    .ON_time(ON_time),
-    .OFF_time(OFF_time),
-    .electrode1(electrode1),
-    .electrode2(electrode2),
-    .enable(enable),
-    .up_switches(up_switches),
-    .down_switches(down_switches),
-    .DAC(DAC),
-    .pulse_active(pulse_active)
-);
-	
+
+aska_dig aska_dig1 (
+			.clk(clk_20khz),   // internal clock 20 kHz
+			.reset_l(resetn), // Reset async. (L)
+            .porborn(porborn), //Power-on-Reset/Brown-out-Reset (L)
+			.SPI_CS(SPI_CS), // chip select  (L)
+			.SPI_Clk(SPI_Clk), // Mode 0, data is sampled at the rising edge
+			.SPI_MOSI(SPI_MOSI), // Master output  Slave Input				
+			.up_switches(up_switches),  // Controls the P switches in the H bridge
+            .down_switches(down_switches), // Controls the N switches in the H bridge
+            .DAC(DAC),
+            .pulse_active(pulse_active));	
 endmodule
--- ASKA_DIG/FPGA_TESTS/top_tb.v
+++ ASKA_DIG/FPGA_TESTS/top_tb.v
@@ -1,20 +1,63 @@
 `include "top.v"
 
 `timescale 1 ns/ 1ps
+//`timescale 1us/ 1ps
+
+`define AMPLITUDE 50
+`define FREQ 400
+`define PHASE 4
+`define RAMP  25
+`define RAMP_FACTOR (`AMPLITUDE*16)/`RAMP
+//`define RAMP_FACTOR 0
+`define ON_TIME  25
+`define OFF_TIME  25
+`define ELE1  32'b0000_0000_0000_0000_1000_0000_0000_0000
+`define ELE2  32'b0000_0000_0000_0000_0100_0000_0000_0000
 
 module top_tb();
 
     reg resetn;
+    reg porborn;    
+    reg SPI_Clk;
+	reg SPI_MOSI;
+	reg SPI_CS;
+
+    // Stimulation parameters
+    reg [5:0] amplitude; //0 - 50 mA
+    reg [11:0] freq; // 4.88 Hz (4095) - 50 Hz (400)
+    reg [2:0] phaseDuration;
+    reg [5:0] ramp;
+    reg [9:0] ramp_factor;
+    reg [7:0] ON_time; // up to 4s (200 for 50 Hz)ramp = `RAMP;    
+    reg [9:0] OFF_time; // up to 12s (600 for 50 Hz)
+    reg [`ELEC_NUM:0] electrode1;
+    reg [`ELEC_NUM:0] electrode2;
     reg enable;
 
-// Main Clk ULX
+    reg [`M-1:0] conf0;
+	reg [`M-1:0] conf1;
+	
+	always @(*) begin
+		    conf0[17:12] = amplitude;
+    	    conf0[11:0] = freq;
+    	    conf1[23:21] = phaseDuration;
+    	    conf0[23:18] = ramp;
+    	    conf1[9:0] = ramp_factor;
+    	    conf0[32:24] = ON_time;
+    	    conf1[19:10] = OFF_time;
+    	    conf1[20] = enable;
+		    conf1[31:24] = 8'b0000_0000;	
+	end
 
-parameter CLK_DELAY = 20;  // 25 MHz
+// Main Clk ULX
+    parameter CLK_DELAY = 20;  // 25 MHz
 
 // Clock Generators:
     reg clk25;
 	always #(CLK_DELAY) clk25 = ~clk25;
 
+    parameter MAIN_CLK_DELAY = 25000;  // 20 kHz
+	parameter SPI_CLK_DELAY = 2; // 500 kHz
 
 //Instantiation of UUT
 		
@@ -29,7 +72,10 @@ parameter CLK_DELAY = 20;  // 25 MHz
 
     always @(*) begin
         gp[0] = resetn;
-        gp[1] = enable;
+        gp[1] = porborn;
+        gp[2] = SPI_Clk;
+        gp[3] = SPI_MOSI;
+        gp[4] = SPI_CS;
     end
 
     wire [2:0] up_switches;
@@ -43,42 +89,210 @@ parameter CLK_DELAY = 20;  // 25 MHz
     assign down_switches = gn[6:4];    
     assign DAC = gn[12:7];
     assign pulse_active = gn[13];
-
     
 	
-	top NPG
+	top top1
 		(	.gp(gp),
 			.gn(gn),
 			.led(led),
 			.btn(btn),
 			.clk_25mhz(clk25)			
 			);
+   
 
+    reg [7:0] TX_data; // Data to send through MOSI	
+	reg [7:0] SPI_Master_RX; // Data received through MISO  
 
 initial begin
     // Required for EDA Playground
 	$dumpfile("dump.vcd"); 
-	$dumpvars;
+	//$dumpvars;
+    $dumpvars(1,top_tb);
 	$display("************************************");
-	$display("Test ASKA NPG");
-    
-    //start
-    clk25 = 0;
-    resetn = 1;
-    enable = 0;
-  
-    //reset async
-    #(5*CLK_DELAY);
-    resetn = 0;
-    #(5*CLK_DELAY);
-    resetn = 1;
+	$display("Test ASKA DIG");
 
-    //enable
-    enable = 1;
+    //Initial State
+		clk25 = 0;
+		TX_data = 8'h00;
+		resetn = 1'b1;
+		porborn = 1'b1;
+		SPI_CS = 1'b1;
+		SPI_Clk = 1'b0;
+		SPI_MOSI = 1'b0;
+        
+        enable = 1'b1;
+		amplitude = `AMPLITUDE;
+		freq = `FREQ;
+		phaseDuration = `PHASE;
+		ramp = `RAMP;
+    	ramp_factor = `RAMP_FACTOR;
+    	ON_time = `ON_TIME;
+    	OFF_time = `OFF_TIME;
+    	electrode1 = `ELE1;
+    	electrode2 = `ELE2;
+ 
+		//Reset
+		#(10*MAIN_CLK_DELAY) resetn = 1'b0;
+		#(10*MAIN_CLK_DELAY) resetn = 1'b1;
+		#(10*MAIN_CLK_DELAY);
+ 
+        send_ASKA(8'h02,electrode1);
+		#(20*MAIN_CLK_DELAY);
+		send_ASKA(8'h03,electrode2);
+        #(20*MAIN_CLK_DELAY);
+		send_ASKA(8'h00,conf0);
+		#(20*MAIN_CLK_DELAY);
+		send_ASKA(8'h01,conf1);
+		
+		#(7500000*SPI_CLK_DELAY); 
 
-    #(16000000*CLK_DELAY); 
+/*
+		enable = 0;
+		#1;	send_ASKA(8'h01,conf1);
+
+		#(5000000*SPI_CLK_DELAY); 
+		ramp = 25;
+		amplitude = 25;
+		ramp_factor = (amplitude*16)/ramp;
+		enable = 1;
+		#1; send_ASKA(8'h00,conf0);		
+		send_ASKA(8'h01,conf1);
+
+		#(10000000*SPI_CLK_DELAY);
+
+		porborn = 0;
+		#(1000000*SPI_CLK_DELAY);
+		porborn = 1;
+
+		#(20000000*SPI_CLK_DELAY); 
+   
+*/
+    #(64000000*CLK_DELAY); 
     $display("************************************");
 	$finish;
 end
+
+
+	task send_ASKA(input [7:0] add, input [31:0] data);
+		begin
+			SPI_CS = 1'b0;
+			#(4*SPI_CLK_DELAY); // models delay between CS and SPI master 
+			
+			send_byte(add);
+			send_byte(data[31:24]);
+			send_byte(data[23:16]);
+			send_byte(data[15:8]);
+			send_byte(data[7:0]);
+			
+			#(4*SPI_CLK_DELAY); // models delay between CS and SPI master 
+			SPI_CS = 1'b1;	
+			
+			//Check values
+			#(4*SPI_CLK_DELAY);
+
+			$display("sent SPI add 0x%X, data 0x%X at time:",add, data,  $time);
+						
+			
+            /*
+			case (add)
+				8'h00:  begin
+							str1 = (data == conf0)? "OK" : "ERROR";
+							$display("sent conf0 0x%X, received 0x%X, %s", data, conf0, str1);
+						end
+				8'h01: 	begin
+							str1 = (data == conf1)? "OK" : "ERROR";
+							$display("sent conf1 0x%X, received 0x%X, %s", data, conf1, str1);
+						end						
+				8'h02:  begin
+							str1 = (data == ele1)? "OK" : "ERROR";
+							$display("sent ele1 0x%X, received 0x%X, %s", data, ele1, str1);	
+						end
+				
+				8'h03: 	begin
+							str1 = (data == ele2)? "OK" : "ERROR";
+							$display("sent ele2 0x%X, received 0x%X, %s", data, ele2, str1); 				
+						end
+						
+			endcase
+			*/
+			
+		end
+	endtask
+
+	task send_ASKA_error(input [7:0] add, input [31:0] data);
+		begin
+			SPI_CS = 1'b0;
+			#(4*MAIN_CLK_DELAY); // models delay between CS and SPI master 
+			
+			send_byte(add);
+			send_byte(data[31:24]);
+			send_byte(data[23:16]);
+			send_byte(data[15:8]);
+			//send_byte(data[7:0]);
+			
+			#(4*MAIN_CLK_DELAY); // models delay between CS and SPI master 
+			SPI_CS = 1'b1;	
+			
+		end
+	endtask
+
+	task send_byte(input [7:0] data);
+		begin
+			TX_data = data;			
+			//#(4*MAIN_CLK_DELAY); // models delay between CS and SPI master 
+			SPI_Clk = 1'b0;
+			SPI_MOSI = TX_data[7];
+						
+			#SPI_CLK_DELAY;		
+			SPI_Clk = 1'b1;
+			#SPI_CLK_DELAY;
+			SPI_Clk = 1'b0;
+			SPI_MOSI = TX_data[6];
+		
+			#SPI_CLK_DELAY;
+			SPI_Clk = 1'b1;			
+			#SPI_CLK_DELAY;
+			SPI_Clk = 1'b0;
+			SPI_MOSI = TX_data[5];
+				
+			#SPI_CLK_DELAY;
+			SPI_Clk = 1'b1;
+			#SPI_CLK_DELAY;
+			SPI_Clk = 1'b0;
+			SPI_MOSI = TX_data[4];
+
+			#SPI_CLK_DELAY;
+			SPI_Clk = 1'b1;
+			#SPI_CLK_DELAY;
+			SPI_Clk = 1'b0;
+			SPI_MOSI = TX_data[3];
+		
+			#SPI_CLK_DELAY;
+			SPI_Clk = 1'b1;			
+			#SPI_CLK_DELAY;
+			SPI_Clk = 1'b0;
+			SPI_MOSI = TX_data[2];
+
+			#SPI_CLK_DELAY;
+			SPI_Clk = 1'b1;			
+			#SPI_CLK_DELAY;
+			SPI_Clk = 1'b0;
+			SPI_MOSI = TX_data[1];
+
+			#SPI_CLK_DELAY;
+			SPI_Clk = 1'b1;			
+			#SPI_CLK_DELAY;
+			SPI_Clk = 1'b0;
+			SPI_MOSI = TX_data[0];
+		
+			#SPI_CLK_DELAY;
+			SPI_Clk = 1'b1;
+			
+			#SPI_CLK_DELAY;
+			SPI_Clk = 1'b0;
+						
+		end
+	endtask
+
 
 endmodule
